4d866a2f9f86abe048af9ecd3b581ebb6fd0a2c0
MathPrm <primotmathilde@gmail.com>
2026-05-05 15:27:47 +0200

    fix: clean files

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Application = express();
 
-app.use(cors());
+const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+
+app.use(cors({
+  origin: frontendUrl
+}));
+
 app.use(express.json());
 
 // "Hello World" test
